d1fb9bea2eff65169db3c8ccde6f5f69eb7bd46b
kimthien2603 <nguyenkimthien2603@gmail.com>
2023-01-01 16:21:50 +0700

    sao ma ko dc

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,8 +6,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["thienhi.com"])
-ALLOWED_HOSTS = ["ec2-175-41-160-73.ap-southeast-1.compute.amazonaws.com"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["thienhi.com"])
+# ALLOWED_HOSTS = ["ec2-175-41-160-73.ap-southeast-1.compute.amazonaws.com"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
