4c1ff0fef861a4b7f066e2210b19444c56dd583c
kimthien2603 <nguyenkimthien2603@gmail.com>
2023-01-03 10:34:00 +0700

    qweqew

--- hotel_manager/facebook/api/facebook_auth_view.py
+++ hotel_manager/facebook/api/facebook_auth_view.py
@@ -33,9 +33,10 @@ class FacebookWebhookView(APIView):
     def get(self, request, format=None, *args, **kwargs):
         hub_mode = request.GET.get('hub.mode')
         hub_challenge = request.GET.get('hub.challenge')
+        challenge = hub_challenge
         hub_verify_token = request.GET.get('hub.verify_token')
         print(f'hub_mode {hub_mode} - hub_challenge {hub_challenge} - hub_verify_token {hub_verify_token}')
-        return Response(str(hub_challenge).replace('"\"',""), status=status.HTTP_200_OK)
+        return Response(data=challenge, status=status.HTTP_200_OK)
 
 
 class VerifyFacebookWebhookView(generics.ListAPIView):
