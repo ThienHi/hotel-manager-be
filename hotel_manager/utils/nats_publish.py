e5723fb549c16b7fe43cf46712448f4998b861ac
kimthien2603 <nguyenkimthien2603@gmail.com>
2023-01-04 08:01:27 +0700

    qwe

--- hotel_manager/utils/nats_publish.py
+++ hotel_manager/utils/nats_publish.py
@@ -2,6 +2,7 @@ import nats
 from django.conf import settings
 
 async def connect_nats_client_publish_websocket(new_topic_publish, data_mid):
+    print(" settings.NATS_URL ========== ", settings.NATS_URL)
     nats_client = await nats.connect(settings.NATS_URL)
     await nats_client.publish(new_topic_publish, bytes(data_mid))
     await nats_client.close()
